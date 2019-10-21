4a7e9a03c3143bfc92dab7d4176d3417383a21d3
Kidane <kidanegidey2018@gmail.com>
2019-10-20 17:11:44 -0700

    replication program

---
+++ helohellohello/src/helohellohello/hellorepl.java
@@ -0,0 +1,24 @@
+package helohellohello;
+
+public class hellorepl {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		repl("kidane", 3);
+		repl("Seattle",0);
+		repl("Central college", -1);
+		
+		
+	}
+
+	public static void repl(String name, int times) {
+		for (int i=1; i<=times; i++) {
+			System.out.print(name);
+			
+		}
+			
+
+	
+	
+    }
+}
